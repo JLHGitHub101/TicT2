5f67fd9a873aee225b921d8c9ad3c6a22b7cef8a
JLHGitHub101 <jlh59@uw.edu>
2023-01-31 21:28:55 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace T3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.R3C3 = new System.Windows.Forms.TextBox();
+            this.R2C3 = new System.Windows.Forms.TextBox();
+            this.R1C3 = new System.Windows.Forms.TextBox();
+            this.R3C2 = new System.Windows.Forms.TextBox();
+            this.R2C2 = new System.Windows.Forms.TextBox();
+            this.R1C2 = new System.Windows.Forms.TextBox();
+            this.R3C1 = new System.Windows.Forms.TextBox();
+            this.R2C1 = new System.Windows.Forms.TextBox();
+            this.R1C1 = new System.Windows.Forms.TextBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Users_Scores = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.R3C3, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.R2C3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.R1C3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.R3C2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.R2C2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.R1C2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.R3C1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.R2C1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.R1C1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(652, 353);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // R3C3
+            // 
+            this.R3C3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R3C3.Location = new System.Drawing.Point(437, 237);
+            this.R3C3.Multiline = true;
+            this.R3C3.Name = "R3C3";
+            this.R3C3.Size = new System.Drawing.Size(211, 111);
+            this.R3C3.TabIndex = 8;
+            this.R3C3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.R3C3.DoubleClick += new System.EventHandler(this.BoxSelection);
+            // 
+            // R2C3
+            // 
+            this.R2C3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R2C3.Location = new System.Drawing.Point(220, 237);
+            this.R2C3.Multiline = true;
+            this.R2C3.Name = "R2C3";
+            this.R2C3.Size = new System.Drawing.Size(211, 111);
+            this.R2C3.TabIndex = 7;
+            this.R2C3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.R2C3.DoubleClick += new System.EventHandler(this.BoxSelection);
+            // 
+            // R1C3
+            // 
+            this.R1C3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R1C3.Location = new System.Drawing.Point(3, 237);
+            this.R1C3.Multiline = true;
+            this.R1C3.Name = "R1C3";
+            this.R1C3.Size = new System.Drawing.Size(211, 111);
+            this.R1C3.TabIndex = 6;
+            this.R1C3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.R1C3.DoubleClick += new System.EventHandler(this.BoxSelection);
+            // 
+            // R3C2
+            // 
+            this.R3C2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R3C2.Location = new System.Drawing.Point(437, 120);
+            this.R3C2.Multiline = true;
+            this.R3C2.Name = "R3C2";
+            this.R3C2.Size = new System.Drawing.Size(211, 111);
+            this.R3C2.TabIndex = 5;
+            this.R3C2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.R3C2.DoubleClick += new System.EventHandler(this.BoxSelection);
+            // 
+            // R2C2
+            // 
+            this.R2C2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R2C2.Location = new System.Drawing.Point(220, 120);
+            this.R2C2.Multiline = true;
+            this.R2C2.Name = "R2C2";
+            this.R2C2.Size = new System.Drawing.Size(211, 111);
+            this.R2C2.TabIndex = 4;
+            this.R2C2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.R2C2.DoubleClick += new System.EventHandler(this.BoxSelection);
+            // 
+            // R1C2
+            // 
+            this.R1C2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R1C2.Location = new System.Drawing.Point(3, 120);
+            this.R1C2.Multiline = true;
+            this.R1C2.Name = "R1C2";
+            this.R1C2.Size = new System.Drawing.Size(211, 111);
+            this.R1C2.TabIndex = 3;
+            this.R1C2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.R1C2.DoubleClick += new System.EventHandler(this.BoxSelection);
+            // 
+            // R3C1
+            // 
+            this.R3C1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R3C1.Location = new System.Drawing.Point(437, 3);
+            this.R3C1.Multiline = true;
+            this.R3C1.Name = "R3C1";
+            this.R3C1.Size = new System.Drawing.Size(211, 111);
+            this.R3C1.TabIndex = 2;
+            this.R3C1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.R3C1.DoubleClick += new System.EventHandler(this.BoxSelection);
+            // 
+            // R2C1
+            // 
+            this.R2C1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R2C1.Location = new System.Drawing.Point(220, 3);
+            this.R2C1.Multiline = true;
+            this.R2C1.Name = "R2C1";
+            this.R2C1.Size = new System.Drawing.Size(211, 111);
+            this.R2C1.TabIndex = 1;
+            this.R2C1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.R2C1.DoubleClick += new System.EventHandler(this.BoxSelection);
+            // 
+            // R1C1
+            // 
+            this.R1C1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R1C1.Location = new System.Drawing.Point(3, 3);
+            this.R1C1.Multiline = true;
+            this.R1C1.Name = "R1C1";
+            this.R1C1.Size = new System.Drawing.Size(211, 111);
+            this.R1C1.TabIndex = 0;
+            this.R1C1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.R1C1.DoubleClick += new System.EventHandler(this.BoxSelection);
+            // 
+            // Start
+            // 
+            this.Start.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Start.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Start.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.Location = new System.Drawing.Point(12, 375);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(214, 124);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.BackColor = System.Drawing.Color.Red;
+            this.Stop.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Stop.Location = new System.Drawing.Point(449, 371);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(215, 128);
+            this.Stop.TabIndex = 2;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = false;
+            // 
+            // Users_Scores
+            // 
+            this.Users_Scores.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Users_Scores.Location = new System.Drawing.Point(232, 375);
+            this.Users_Scores.Multiline = true;
+            this.Users_Scores.Name = "Users_Scores";
+            this.Users_Scores.Size = new System.Drawing.Size(211, 36);
+            this.Users_Scores.TabIndex = 3;
+            this.Users_Scores.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 511);
+            this.Controls.Add(this.Users_Scores);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximumSize = new System.Drawing.Size(700, 550);
+            this.MinimumSize = new System.Drawing.Size(400, 375);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.TextBox R3C3;
+        private System.Windows.Forms.TextBox R2C3;
+        private System.Windows.Forms.TextBox R1C3;
+        private System.Windows.Forms.TextBox R3C2;
+        private System.Windows.Forms.TextBox R2C2;
+        private System.Windows.Forms.TextBox R1C2;
+        private System.Windows.Forms.TextBox R3C1;
+        private System.Windows.Forms.TextBox R2C1;
+        private System.Windows.Forms.TextBox R1C1;
+        private System.Windows.Forms.TextBox Users_Scores;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T3
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+ 
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            var rand = new Random();
+            int humanuser = rand.Next(1,11);
+            int computer = rand.Next(1,11);
+            int firstmove;
+
+            do
+            { 
+                firstmove= rand.Next(1,11);
+            }
+            while (!((firstmove == humanuser) || (firstmove == computer)));
+
+            if (firstmove == humanuser)
+            {
+                Users_Scores.Text = "Human 1st";
+            }
+            else
+            {
+                Users_Scores.Text = "Computer 1st";
+            }
+     
+
+        }
+        private void BoxSelection(object sender, EventArgs e)
+        {
+           TextBox user =  (TextBox)sender;
+            user.Text = "X";
+        }
+
+   
+    }
+}
